1c68f3eb725ad377371fa96459a62ba0d02f5a53
Andrew Leggett <me@andyleggett.co.uk>
2015-06-10 16:28:51 +0100

    Trying to find good examples

--- 02-variables.js
+++ 02-variables.js
@@ -3,11 +3,21 @@ var console = $('#console');
 var aNumber = 0; //integer
 var anotherNumber = 0.0; //float
 
-var aString = 'This is a string';
-
-var aBoolean = true;
-
-var aNumberWithNoInitalValue;
-aNumberWithNoInitialValue = 12;
-
 console.text(aNumber);
+console.text(anotherNumber);
+
+//var aString = 'This is a string';
+
+//console.text(aString);
+
+//var aBoolean = true;
+
+//console.text(aBoolean);
+
+//var aNumberWithNoInitalValue;
+
+//console.text(aNumberWithNoInitalValue);
+
+//aNumberWithNoInitialValue = 12;
+
+//console.text(aNumberWithNoInitalValue);
--- 12-loops.js
+++ 12-loops.js
@@ -1,37 +1,31 @@
 var console = $('#console');
 
-var loop1;
-var loop2;
-var loop3;
+var counterLoop;
+var counter = 0;
 
-
-for (loop1 = 0; loop1 < 20; loop1 += 1){
-	//do stuff with loop1 = 0
-	//do stuff with loop1 = 1
-	//do stuff with loop1 = 2
-	//...
-	//do stuff with loop1 = 18
-	//do stuff with loop1 = 19
+for (counterLoop = 0; counterLoop < 20; counterLoop += 1){
+	counter += 1;
 }
 
 
-for (loop2 = 100; loop2 > 0; loop2 -= 1){
-	//do stuff with loop1 = 100
-	//do stuff with loop1 = 99
-	//do stuff with loop1 = 98
-	//...
-	//do stuff with loop1 = 2
-	//do stuff with loop1 = 1
+var calcLoop;
+var results = [];
+
+for (calcLoop = 0; calcLoop < 10; calcLoop += 1){
+	results[calcLoop] = 2 * calcLoop + 1;
+}
+
+
+var reverseLoop;
+var countDownText = 'Countdown ... ';
+
+for (reverseLoop = 10; reverseLoop > 0; reverseLoop -= 1){
+	countDownText = countdownText + reverseLoop + ' ';
 }
 
 
 for (loop3 = 10; loop3 < 50; loop3 += 5){
-	//do stuff with loop1 = 10
-	//do stuff with loop1 = 15
-	//do stuff with loop1 = 20
-	//...
-	//do stuff with loop1 = 40
-	//do stuff with loop1 = 45
+
 }
 
 
